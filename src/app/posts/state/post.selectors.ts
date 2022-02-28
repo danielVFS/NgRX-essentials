6001938b2a7bc8b558b92d4337180b80154732e0
danielVFS <daniel.vitor@cuia.com.br>
2022-02-28 19:20:04 -0300

    update on  post selector

--- src/app/posts/state/post.selectors.ts
+++ src/app/posts/state/post.selectors.ts
@@ -3,7 +3,12 @@ import { PostsState } from './posts.state';
 
 const getPostsState = createFeatureSelector<PostsState>('posts');
 
-export const getPosts = createSelector(
-  getPostsState,
-  (state: PostsState) => state.posts
-);
+export const getPosts = () => {
+  return createSelector(getPostsState, (state: PostsState) => state.posts);
+};
+
+export const getPostById = (props: { id: string }) => {
+  return createSelector(getPostsState, (state: PostsState) =>
+    state.posts.find((post) => post.id === props.id)
+  );
+};
