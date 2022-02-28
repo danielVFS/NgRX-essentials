ff384f6bc5735d78282da81fa0452e4dc472b9f1
danielVFS <daniel.vitor@cuia.com.br>
2022-02-28 14:20:13 -0300

    posts list

--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/counter/app.state';
+import { Post } from 'src/app/models/post.model';
+import { getPosts } from '../state/post.selectors';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
-  styleUrls: ['./posts-list.component.scss']
+  styleUrls: ['./posts-list.component.scss'],
 })
 export class PostsListComponent implements OnInit {
+  posts$: Observable<Post[]>;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.posts$ = this.store.select(getPosts);
   }
-
 }
