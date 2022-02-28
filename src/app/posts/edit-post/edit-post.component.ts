463a2d6c28bc14841040ac962a1f7f15144b688c
danielVFS <daniel.vitor@cuia.com.br>
2022-02-28 19:30:05 -0300

    get posts

--- src/app/posts/edit-post/edit-post.component.ts
+++ src/app/posts/edit-post/edit-post.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { AppState } from 'src/app/counter/app.state';
 import { Post } from 'src/app/models/post.model';
 import { getPostById } from '../state/post.selectors';
@@ -10,16 +12,42 @@ import { getPostById } from '../state/post.selectors';
   templateUrl: './edit-post.component.html',
   styleUrls: ['./edit-post.component.scss'],
 })
-export class EditPostComponent implements OnInit {
+export class EditPostComponent implements OnInit, OnDestroy {
   postId: string;
   post: Post;
+  postForm: FormGroup;
+  postSubscription$: Subscription;
 
   constructor(private route: ActivatedRoute, private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((param) => (this.postId = param.get('id')));
-    this.store
-      .select(getPostById({ id: this.postId }))
-      .subscribe((data) => console.log(data));
+    this.route.paramMap.subscribe((param) => {
+      this.postId = param.get('id');
+      this.postSubscription$ = this.store
+        .select(getPostById({ id: this.postId }))
+        .subscribe((data) => {
+          this.post = data;
+          this.createForm();
+        });
+    });
   }
+
+  createForm(): void {
+    this.postForm = new FormGroup({
+      title: new FormControl(this.post.title, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      description: new FormControl(this.post.description, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.postSubscription$) this.postSubscription$.unsubscribe();
+  }
+
+  onUpdatePost(): void {}
 }
