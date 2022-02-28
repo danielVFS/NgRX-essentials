8eb9192bedcb04ae8f39414f656acb15636484e8
danielVFS <daniel.vitor@cuia.com.br>
2022-02-28 15:10:20 -0300

    open add post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,13 @@ import { CounterOutputComponent } from './counter/counter-output/counter-output.
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { StoreModule } from '@ngrx/store';
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
 import { environment } from 'src/environments/environment';
 import { appReducer } from './counter/app.state';
+import { AddPostComponent } from './posts/add-post/add-post.component';
 
 @NgModule({
   declarations: [
@@ -26,11 +27,13 @@ import { appReducer } from './counter/app.state';
     HomeComponent,
     HeaderComponent,
     PostsListComponent,
+    AddPostComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
 
     StoreModule.forRoot(appReducer),
     StoreDevtoolsModule.instrument({
